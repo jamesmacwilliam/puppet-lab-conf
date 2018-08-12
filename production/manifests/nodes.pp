87f20b5ce9c73c14b52fde43b49d522636db22c4
James Mac William <jimmy.macwilliam@gmail.com>
2018-08-12 00:49:04 -0400

    admin tools packages

--- production/manifests/nodes.pp
+++ production/manifests/nodes.pp
@@ -1,22 +1,28 @@
 class linux {
-  file { '/info.txt':
-    ensure  => 'present',
-    content => inline_template("Created by Puppet at <%= Time.now %>\n")
-  }
-
-  package { 'ntp':
-    ensure => 'installed'
-  }
-
   $ntpservice = $osfamily ? {
     'redhat' => 'ntpd',
     'debian' => 'ntp',
     default  => 'ntp'
   }
 
+  $admintools ['git', 'vim', 'screen']
+
+  package { $admintools:
+    ensure => 'installed'
+  }
+
+  package { 'ntp':
+    ensure => 'installed'
+  }
+
   service { $ntpservice:
     ensure  => 'running',
     enable  => true
+  }
+
+  file { '/info.txt':
+    ensure  => 'present',
+    content => inline_template("Created by Puppet at <%= Time.now %>\n")
   }
 }
 
