bd26cab3436a41b6883acbce1533cabd4fcefe18
James Mac William <jimmy.macwilliam@gmail.com>
2018-08-12 22:42:46 -0400

    use hiera to manage classes
    - hiera_include in nodes.pp
    - extract linux to it's own class outside of nodes.pp

---
+++ linux/manifests/init.pp
@@ -0,0 +1,27 @@
+class linux {
+  $ntpservice = $osfamily ? {
+    'redhat' => 'ntpd',
+    'debian' => 'ntp',
+    default  => 'ntp'
+  }
+
+  $admintools = ['git', 'nano', 'screen']
+
+  package { $admintools:
+    ensure => 'installed'
+  }
+
+  package { 'ntp':
+    ensure => 'installed'
+  }
+
+  service { $ntpservice:
+    ensure  => 'running',
+    enable  => true
+  }
+
+  file { '/info.txt':
+    ensure  => 'present',
+    content => inline_template("Created by Puppet at <%= Time.now %>\n")
+  }
+}
--- production/manifests/nodes.pp
+++ production/manifests/nodes.pp
@@ -1,37 +1,7 @@
-class linux {
-  $ntpservice = $osfamily ? {
-    'redhat' => 'ntpd',
-    'debian' => 'ntp',
-    default  => 'ntp'
-  }
-
-  $admintools = ['git', 'nano', 'screen']
-
-  package { $admintools:
-    ensure => 'installed'
-  }
-
-  package { 'ntp':
-    ensure => 'installed'
-  }
-
-  service { $ntpservice:
-    ensure  => 'running',
-    enable  => true
-  }
-
-  file { '/info.txt':
-    ensure  => 'present',
-    content => inline_template("Created by Puppet at <%= Time.now %>\n")
-  }
-}
-
 node 'wiki' {
-  class { 'linux': }
-  class { 'mediawiki': }
+  hiera_include('classes')
 }
 
 node 'wikitest' {
-  class { 'linux': }
-  class { 'mediawiki': }
+  hiera_include('classes')
 }
